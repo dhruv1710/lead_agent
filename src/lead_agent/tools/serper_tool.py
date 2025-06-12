439e4a236b429be189cea94c5683dce9f84f1feb
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2025-06-12 13:24:20 +0530

    Update serper_tool.py

--- src/lead_agent/tools/serper_tool.py
+++ src/lead_agent/tools/serper_tool.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from crewai_tools import SerperDevTool
 load_dotenv()
-os.environ['SERPER_API_KEY'] = 'd61d6e06a4e708547ead52468cd5c3d1678fb7f4'
+os.environ['SERPER_API_KEY'] = os.getenv('SERPER_API_KEY')
 
 # inititlaize the tool for internet searching capabilities
 serper_tool = SerperDevTool()
